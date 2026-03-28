35aa8353585935bfccfe64ae4c9f8088faa44d85
delizer-r <dmitrvas2004@gmail.com>
2026-03-28 13:48:29 +0300

    second part completed

--- index.php
+++ index.php
@@ -36,4 +36,41 @@ if(file_exists("folder/mir.txt")){
 } else {
     echo "mir.txt не существует<br>";
 }
+
+if(!file_exists("test")){
+    mkdir("test");
+    echo "Папка 'test' создана<br>";
+}
+
+rename("test", "www");
+echo "Папка 'test' переименована в 'www'<br>";
+
+rmdir("www");
+echo "Папка 'www' удалена<br>";
+
+if(!file_exists("test")){
+    mkdir("test");
+    echo "Папка 'test' создана заново<br>";
+}
+
+$arr = ["folder1", "folder2", "folder3"];
+foreach($arr as $folder){
+    if(!file_exists("test/" . $folder)){
+        mkdir("test/" . $folder);
+        echo "Папка 'test/$folder' создана<br>";
+    }
+}
+
+$jpgFiles = ["image1.jpg", "photo2.jpg", "picture3.jpg", "document.txt", "style.css"];
+foreach($jpgFiles as $file){
+    $handle = fopen($file, "w");
+    fwrite($handle, "Тестовое содержимое файла $file");
+    fclose($handle);
+    echo "Файл '$file' создан<br>";
+}
+
+echo "Файлы с расширением jpg из текущей папки:<br>";
+foreach(glob("*.jpg") as $file){
+    echo basename($file) . "<br>";
+}
 ?>
