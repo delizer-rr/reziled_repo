5d1c97b345092abe391ce25ad4e12093eaa367e2
delizer-r <dmitrvas2004@gmail.com>
2026-04-30 19:18:42 +0300

    вроде все

---
+++ env.php
@@ -0,0 +1,17 @@
+# .env.example — шаблон, можно коммитить
+
+# 🗄️ PostgreSQL
+PGHOST=127.0.0.1
+PGPORT=5432
+PGDATABASE=vasilev
+PGUSER=postgres
+PGPASSWORD=1234
+
+# 🌐 Приложение
+APP_ENV=development
+APP_URL=http://vasilev.com
+APP_DEBUG=true
+
+# 🔌 PHP-FPM / NGINX (опционально)
+FASTCGI_HOST=127.0.0.1
+FASTCGI_PORT=9000
